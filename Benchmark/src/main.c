84bcaa64415a512416231b2bfd8d512fbede2f32
Frank Elsinga <frank@elsinga.de>
2021-04-19 21:26:07 +0200

    fixed c-benchmarking

--- Benchmark/src/main.c
+++ Benchmark/src/main.c
@@ -1,48 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-#define BENCHMARK_NUM 100
-#define BENCHMARK_REP 1000000
+
+#define BENCHMARK_NUM 10
+#define BENCHMARK_REP 10000000
 #define BENCHMARK_LONG_REP 100000000
 
-int *generate_random_arr(int size)
+int* generate_random_arr(long size)
 {
-    int *arr = malloc(sizeof(int) * size);
-    for (int i = 0; i < size; i++)
-    {
+    int* arr = malloc(sizeof(int) * size);
+    for (long i = 0; i < size; i++) {
         arr[i] = rand();
     }
+    return arr;
 }
 
-void sum(int *arr)
+void sum(int* arr)
 {
     long sum = 0;
-    for (int i = 0; i < BENCHMARK_LONG_REP; i++)
-    {
+    for (long i = 0; i < BENCHMARK_LONG_REP; i++) {
         sum += arr[i];
     }
+    if (sum == 0)
+        exit(EXIT_FAILURE);
 }
 
-void sum_if(int *arr)
+void sum_if(int* arr)
 {
     long sum = 0;
-    for (int i = 0; i < BENCHMARK_REP; i++)
-    {
+    for (long i = 0; i < BENCHMARK_REP; i++) {
         if (arr[i] > 1073741823)
             sum += arr[i];
     }
+    if (sum == 0)
+        exit(EXIT_FAILURE);
 }
 
-int *no_duplicate(int *arr)
+void no_duplicate(int* arr)
 {
-    int len = BENCHMARK_REP;
-    for (int i = 0; i < len; i++)
-    {
-        for (int j = i + 1; j < len; j++)
-        {
-            if (arr[i] == arr[j])
-            {
-                for (int k = j; k < len; k++)
-                {
+    int len = 100;
+    for (long i = 0; i < len; i++) {
+        for (long j = i + 1; j < len; j++) {
+            if (arr[i] == arr[j]) {
+                for (long k = j; k < len; k++) {
                     arr[k] = arr[k + 1];
                 }
                 j--;
@@ -50,39 +50,48 @@ int *no_duplicate(int *arr)
             }
         }
     }
-    return arr;
+
+    if (len == BENCHMARK_REP)
+        exit(EXIT_FAILURE);
 }
 
-void main()
+int main()
 {
-    int *benchmark_data = generate_random_arr(BENCHMARK_REP);
-    int *long_benchmark = generate_random_arr(BENCHMARK_LONG_REP);
-printf("[[");
-    for (int i = 0; i < 100; i++)
-    {
-    clock_t begin = clock();
-    sum(long_benchmark);
-clock_t end = clock();
-printf("%d, " ,(double)(end - begin) / CLOCKS_PER_SEC);
+    struct timespec start;
+    struct timespec end;
+    printf("[[");
+    for (int i = 0; i < 100; i++) {
+        int* long_benchmark = generate_random_arr(BENCHMARK_LONG_REP);
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        sum(long_benchmark);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        printf("%f", (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) * 1e-9);
+        if (i + 1 < 100)
+            printf(", ");
+        free(long_benchmark);
     }
-printf("], [");
-    for (int i = 0; i < 100; i++)
-    {
-    clock_t begin = clock();
-    sum_if(benchmark_data);
-clock_t end = clock();
-printf("%d, " ,(double)(end - begin) / CLOCKS_PER_SEC);
+    printf("],\n[");
+    for (int i = 0; i < 100; i++) {
+        int* benchmark_data = generate_random_arr(BENCHMARK_REP);
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        sum_if(benchmark_data);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        printf("%f", (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) * 1e-9);
+        if (i + 1 < 100)
+            printf(", ");
+        free(benchmark_data);
     }
-
-printf("], [");
-    for (int i = 0; i < 100; i++)
-    {
-    clock_t begin = clock();
-    no_duplicate(benchmark_data);
-clock_t end = clock();
-printf("%d, " ,(double)(end - begin) / CLOCKS_PER_SEC);
+    printf("],\n[");
+    for (int i = 0; i < 100; i++) {
+        int* benchmark_data = generate_random_arr(BENCHMARK_REP);
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        no_duplicate(benchmark_data);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        printf("%f", (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) * 1e-9);
+        if (i + 1 < 100)
+            printf(", ");
+        free(benchmark_data);
     }
-printf("]]");
-    free(benchmark_data);
-    free(long_benchmark);
+    printf("]]\n");
+    return EXIT_SUCCESS;
 }
